c5f74390eecfc9bc80005141eac41e00a47277a9
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2020-03-04 08:56:15 +0300

    add: admin module

--- culture/src/app/app-routing.module.ts
+++ culture/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'author', loadChildren: () => import('./author/author.module').then(m => m.AuthorModule) },
-  { path: 'team', loadChildren: () => import('./team/team.module').then(m => m.TeamModule) }
+  { path: 'team', loadChildren: () => import('./team/team.module').then(m => m.TeamModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
 ];
 
 @NgModule({
